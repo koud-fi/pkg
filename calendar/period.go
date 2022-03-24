4b426f5d908b9067572f7a394c26fa90dc47d7af
Pasi Hyvönen <pasi@koud.fi>
2022-03-24 07:25:44 +0200

    Add TODO

--- calendar/period.go
+++ calendar/period.go
@@ -25,6 +25,9 @@ func Normalize(ps []MutablePeriod) error {
 		return ps[i].Start().Before(ps[j].Start())
 	})
 	for i := range ps {
+
+		// TODO: add options to nil end if it's same as next periods start?
+
 		if ps[i].End() == nil {
 			next := i + 1
 			if next > len(ps)-1 {
