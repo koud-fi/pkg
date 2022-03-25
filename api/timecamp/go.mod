cfeea7d2024d3a348d11cce845a8da6ecd3b0792
Pasi Hyvönen <pasi@koud.fi>
2022-03-25 07:24:55 +0200

    Add timecamp.Client with CreateEntries method

--- api/timecamp/go.mod
+++ api/timecamp/go.mod
@@ -2,4 +2,10 @@ module github.com/koud-fi/pkg/api/timecamp
 
 go 1.18
 
-require github.com/koud-fi/pkg/calendar v0.0.0-20220324063507-69e3ed40579e
+require (
+	github.com/koud-fi/pkg/blob v0.0.0-20220323040432-4bae881660c8
+	github.com/koud-fi/pkg/calendar v0.0.0-20220324063507-69e3ed40579e
+	github.com/koud-fi/pkg/fetch v0.0.0-20220324064510-4720bb485366
+)
+
+require golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
--- api/timecamp/timecamp.go
+++ api/timecamp/timecamp.go
@@ -1,10 +1,18 @@
 package timecamp
 
 import (
+	"errors"
+	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 
+	"github.com/koud-fi/pkg/blob"
 	"github.com/koud-fi/pkg/calendar"
+	"github.com/koud-fi/pkg/fetch"
 )
+
+const apiRoot = "https://app.timecamp.com/third_party/api"
 
 var _ calendar.MutablePeriod = (*Entry)(nil)
 
@@ -20,4 +28,35 @@ func (e Entry) End() *time.Time       { return e.EndTime }
 func (e *Entry) SetStart(t time.Time) { e.StartTime = t }
 func (e *Entry) SetEnd(t *time.Time)  { e.EndTime = t }
 
-// TODO
+type Client struct {
+	baseReq *fetch.Request
+}
+
+func New(apiKey string) *Client {
+	return &Client{baseReq: fetch.New().Authorization(apiKey)}
+}
+
+func (c Client) CreateEntries(e ...Entry) error {
+	for _, e := range e {
+		if e.EndTime == nil {
+			return errors.New("missing end time")
+		}
+		if err := blob.Error(c.baseReq.
+			Method(http.MethodPost).
+			URL(apiRoot + "/entries").
+			Form(url.Values{
+				"task_id":    []string{strconv.FormatInt(e.TaskID, 10)},
+				"date":       []string{e.StartTime.Format("2006-01-02")},
+				"start_time": []string{e.StartTime.Format(time.RFC3339)},
+				"end_time":   []string{e.EndTime.Format(time.RFC3339)},
+				"note":       []string{e.Note},
+			}),
+		); err != nil {
+			return err
+		}
+	}
+
+	// TODO: return IDs of the created entries?
+
+	return nil
+}
