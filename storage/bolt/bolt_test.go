5b852b2e951a825a4e8f3822db2d884f0afd816d
Pasi Hyvönen <pasi@koud.fi>
2022-03-27 10:16:36 +0300

    Add bolt test

---
+++ storage/bolt/bolt_test.go
@@ -0,0 +1,20 @@
+package bolt_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/koud-fi/pkg/storage/bolt"
+	"github.com/koud-fi/pkg/storage/storagetest"
+)
+
+func Test(t *testing.T) {
+	os.RemoveAll("temp")
+	db, err := bolt.Open("temp/test.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	storagetest.Test(t, bolt.NewStorage(db, "test"))
+}
