bd75e6ce0b9bc556a325b73719b119a9e2084118
Pasi Hyvönen <pasi@koud.fi>
2022-04-16 13:42:11 +0300

    Add cas test

---
+++ cas/cas_test.go
@@ -0,0 +1,54 @@
+package cas_test
+
+import (
+	"encoding/json"
+	"io/fs"
+	"os"
+	"testing"
+
+	"github.com/koud-fi/pkg/blob"
+	"github.com/koud-fi/pkg/blob/localfile"
+	"github.com/koud-fi/pkg/cas"
+	"github.com/koud-fi/pkg/grf"
+	"github.com/koud-fi/pkg/grf/memgrf"
+	"github.com/koud-fi/pkg/storage/localdisk"
+)
+
+func Test(t *testing.T) {
+	var (
+		fsys = os.DirFS("../testdata")
+		gm   = memgrf.NewMapper()
+		gs   = memgrf.NewStore()
+	)
+	bs, err := localdisk.NewStorage("temp/file")
+	assert(t, err)
+
+	s := cas.New(bs, grf.New(gm, gs), "file")
+	assert(t, fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		n, err := s.Add(blob.FromFS(fsys, path))
+		if err != nil {
+			return err
+		}
+		t.Logf("%s %v", path, n)
+		return nil
+	}))
+	keyMapData, err := json.MarshalIndent(gm, "", "\t")
+	assert(t, err)
+	assert(t, localfile.Write("temp/keymap.json", blob.FromBytes(keyMapData), 0600))
+
+	graphData, err := json.MarshalIndent(gs, "", "\t")
+	assert(t, err)
+	assert(t, localfile.Write("temp/graph.json", blob.FromBytes(graphData), 0600))
+}
+
+func assert(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
