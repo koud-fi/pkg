2f20f530d71e629a15717ff475e6f11ddae4fd9d
Pasi Hyvönen <pasi@koud.fi>
2022-04-17 19:51:12 +0300

    Add property accessors to edge

--- grf/edge.go
+++ grf/edge.go
@@ -1,13 +1,27 @@
 package grf
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type Edge struct {
-	d EdgeData
+	from ID
+	d    EdgeData
 }
 
 type EdgeType string
 
-func (e Edge) Data(v any) error {
+func (e Edge) From() ID        { return e.from }
+func (e Edge) Type() EdgeType  { return e.d.Type }
+func (e Edge) To() ID          { return e.d.To }
+func (e Edge) Sequence() int64 { return e.d.Sequence }
+
+func (e Edge) Unmarshal(v any) error {
 	return json.Unmarshal(e.d.Data, v)
+}
+
+func (e Edge) String() string {
+	return fmt.Sprintf("%d>%s>%d(%d) %s",
+		e.from, e.d.Type, e.d.To, e.d.Sequence, string(e.d.Data))
 }
