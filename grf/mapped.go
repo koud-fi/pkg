6b49792fa8e8d28dd1b19f9810aea873ff3f2880
Pasi Hyvönen <pasi@koud.fi>
2022-04-21 07:21:34 +0300

    Add mapped node handling function stumps

--- grf/mapped.go
+++ grf/mapped.go
@@ -1,19 +1,40 @@
 package grf
 
-/*
-func (g *Graph) MappedNode(nt NodeType, key string, add bool) *Node {
-	id, err := g.m.Map(nt, key)
-	if err != nil {
-		if add && err == ErrNotFound {
-			n, err := g.AddNode(nt, nil)
-			if err != nil {
-				return &Node{err: err}
-			}
-			n.err = g.m.SetMapping(nt, key, n.id)
-			return n
-		}
-		return &Node{err: err}
-	}
-	return g.Node(id)
+func Mapped[T any](g *Graph, nt NodeType, key string) (*Node[T], error) {
+
+	// ???
+
+	panic("TODO")
 }
+
+func SetMapped[T any](
+	g *Graph, nt NodeType, key string, fn func(T) (T, error),
+) (*Node[T], error) {
+
+	// ???
+
+	panic("TODO")
+}
+
+func DeleteMapped(g *Graph, nt NodeType, key ...string) error {
+
+	// ???
+
+	panic("TODO")
+}
+
+/*
+id, err := g.m.Map(nt, key)
+if err != nil {
+	if add && err == ErrNotFound {
+		n, err := g.AddNode(nt, nil)
+		if err != nil {
+			return &Node{err: err}
+		}
+		n.err = g.m.SetMapping(nt, key, n.id)
+		return n
+	}
+	return &Node{err: err}
+}
+return g.Node(id)
 */
