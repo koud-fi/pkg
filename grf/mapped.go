76d0af455418ebb97e3e1eda966bcad63677c242
Pasi Hyvönen <pasi@koud.fi>
2022-04-21 07:29:22 +0300

    Implement mapped node lookup

--- grf/mapped.go
+++ grf/mapped.go
@@ -1,37 +1,32 @@
 package grf
 
 func Mapped[T any](g *Graph, nt NodeType, key string) (*Node[T], error) {
-
-	// ???
-
-	panic("TODO")
+	id, err := g.m.Map(nt, key)
+	if err != nil {
+		return nil, err
+	}
+	return Lookup[T](g, id)
 }
 
 func SetMapped[T any](
 	g *Graph, nt NodeType, key string, fn func(T) (T, error),
 ) (*Node[T], error) {
 
-	// ???
-
-	panic("TODO")
-}
-
-func DeleteMapped(g *Graph, nt NodeType, key ...string) error {
-	return g.m.DeleteMapping(nt, key...)
-}
-
-/*
-id, err := g.m.Map(nt, key)
-if err != nil {
-	if add && err == ErrNotFound {
+	/*
 		n, err := g.AddNode(nt, nil)
 		if err != nil {
 			return &Node{err: err}
 		}
 		n.err = g.m.SetMapping(nt, key, n.id)
 		return n
-	}
-	return &Node{err: err}
+	*/
+
+	panic("TODO")
 }
-return g.Node(id)
-*/
+
+func DeleteMapped(g *Graph, nt NodeType, key ...string) error {
+
+	// TODO: delete the actual nodes
+
+	return g.m.DeleteMapping(nt, key...)
+}
