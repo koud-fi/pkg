29d1e73ed7f128c904d7fd806b77aacfae979e3a
Pasi Hyvönen <pasi@koud.fi>
2022-04-22 07:43:20 +0300

    Add dataIter

--- iter/iter.go
+++ iter/iter.go
@@ -5,3 +5,20 @@ type Iter[T any] interface {
 	Value() T
 	Close() error
 }
+
+func From[T any](data ...T) Iter[T] {
+	return &dataIter[T]{data: data, offset: -1}
+}
+
+type dataIter[T any] struct {
+	data   []T
+	offset int
+}
+
+func (it *dataIter[T]) Next() bool {
+	it.offset++
+	return it.offset < len(it.data)
+}
+
+func (it dataIter[T]) Value() T     { return it.data[it.offset] }
+func (it dataIter[T]) Close() error { return nil }
