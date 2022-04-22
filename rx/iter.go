38d36640a47d49d1a277c9fde1c0310ce515e0e9
Pasi Hyvönen <pasi@koud.fi>
2022-04-22 17:28:19 +0300

    Add FuncIter

--- rx/iter.go
+++ rx/iter.go
@@ -7,20 +7,41 @@ type Iter[T any] interface {
 }
 
 func SliceIter[T any](data ...T) Iter[T] {
-	return &sliceIter[T]{data: data, offset: -1}
+	return &sliceIter[T]{data: data}
 }
 
-type sliceIter[T any] struct {
-	data   []T
-	offset int
-}
+type sliceIter[T any] struct{ data []T }
 
 func (it *sliceIter[T]) Next() bool {
-	it.offset++
-	return it.offset < len(it.data)
+	if len(it.data) == 0 {
+		return false
+	}
+	it.data = (it.data)[:len(it.data)-1]
+	return len(it.data) > 0
 }
 
-func (it sliceIter[T]) Value() T     { return it.data[it.offset] }
+func (it sliceIter[T]) Value() T     { return it.data[0] }
 func (it sliceIter[T]) Close() error { return nil }
 
-func FromFunc()
+func FuncIter[T any](fn func() ([]T, error)) Iter[T] {
+	return &funcIter[T]{fn: fn}
+}
+
+type funcIter[T any] struct {
+	fn func() ([]T, error)
+	sliceIter[T]
+	err error
+}
+
+func (it *funcIter[T]) Next() bool {
+	if it.err != nil {
+		return false
+	}
+	if it.sliceIter.Next() {
+		return true
+	}
+	it.data, it.err = it.fn()
+	return len(it.data) > 0 && it.err == nil
+}
+
+func (it funcIter[T]) Close() error { return it.err }
