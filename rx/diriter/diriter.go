ca197e24409e37b9f0e27552cd99f7f01ae378d1
Pasi Hyvönen <pasi@koud.fi>
2022-04-24 09:14:42 +0300

    Make diriter work like it should

--- rx/diriter/diriter.go
+++ rx/diriter/diriter.go
@@ -2,21 +2,26 @@ package diriter
 
 import (
 	"io/fs"
-	"path"
 
 	"github.com/koud-fi/pkg/rx"
 )
 
 func New(fsys fs.FS, root string) rx.Iter[string] {
 	return rx.FuncIter(func() ([]string, bool, error) {
-		dir, err := fs.ReadDir(fsys, root)
-		if err != nil {
-			return nil, false, err
-		}
-		out := make([]string, 0, len(dir))
-		for _, d := range dir {
-			out = append(out, path.Join(root, d.Name()))
-		}
-		return out, false, nil
+
+		// TODO: proper lazy implementation
+
+		var paths []string
+		err := fs.WalkDir(fsys, root, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if d.IsDir() {
+				return nil
+			}
+			paths = append(paths, path)
+			return nil
+		})
+		return paths, false, err
 	})
 }
