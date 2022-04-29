7e76bc3078b74d0dedbcb9fa41e545f22afcfbe7
Pasi Hyvönen <pasi@koud.fi>
2022-04-29 06:37:19 +0300

    Add num.Clamp function

--- num/normalize.go
+++ num/normalize.go
@@ -1,8 +1,21 @@
 package num
 
-import "math"
+import (
+	"math"
+
+	"golang.org/x/exp/constraints"
+)
 
 // Gompertz function based on https://en.wikipedia.org/wiki/Gompertz_function
 func Gompertz(t, a, b, c float64) float64 {
 	return a * math.Pow(math.E, b*math.Pow(math.E, c*t))
+}
+
+func Clamp[T constraints.Ordered](n, min, max T) T {
+	if n < min {
+		return min
+	} else if n > max {
+		return max
+	}
+	return n
 }
