401d235d293524798800c721cf1c8eea9bb9c5e7
Pasi Hyvönen <pasi@koud.fi>
2022-05-05 07:42:51 +0300

    Add worlds shitties router implementation

--- router/router.go
+++ router/router.go
@@ -1,3 +1,20 @@
 package router
 
-// TODO
+type Router struct {
+	routes map[string]any
+}
+
+func New() *Router {
+	return &Router{routes: make(map[string]any)}
+}
+
+func (r *Router) Add(route string, handler any) {
+	r.routes[route] = handler
+}
+
+func (r Router) Lookup(route string) (any, func(string) string) {
+
+	// TODO: support route parameters
+
+	return r.routes[route], func(key string) string { return "" }
+}
