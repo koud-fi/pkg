b147fb1c51af033b63af438f234686914aeeae70
Pasi Hyvönen <pasi@koud.fi>
2022-05-06 07:28:18 +0300

    route -> endpoint, add validator

--- proc/router.go
+++ proc/router.go
@@ -3,39 +3,48 @@ package proc
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"sort"
 	"strings"
 )
 
+const endpointPattern = `^[\.a-zA-Z0-9]+$`
+
+var endpointValidator = regexp.MustCompile(endpointPattern)
+
 type Router struct {
-	routeMap map[string]Proc
+	endpoints map[string]Proc
 }
 
 func NewRouter() Router {
-	return Router{routeMap: make(map[string]Proc)}
+	return Router{endpoints: make(map[string]Proc)}
 }
 
-func (r *Router) Add(route string, pr Proc) {
-	r.routeMap[normalizeRoute(route)] = pr
+func (r *Router) Add(endpoint string, pr Proc) {
+	if !endpointValidator.MatchString(endpoint) {
+		panic(fmt.Sprintf("%s doesn't match endpoint pattern: %s",
+			endpoint, endpointPattern))
+	}
+	r.endpoints[normalizeEndpoint(endpoint)] = pr
 }
 
 func (r Router) Invoke(ctx context.Context, route string, p Params) (any, error) {
-	pr, ok := r.routeMap[normalizeRoute(route)]
+	pr, ok := r.endpoints[normalizeEndpoint(route)]
 	if !ok {
 		return nil, fmt.Errorf("not found: %s", route)
 	}
 	return pr.Invoke(ctx, p)
 }
 
-func (r Router) Routes() []string {
-	rs := make([]string, 0, len(r.routeMap))
-	for r := range r.routeMap {
-		rs = append(rs, r)
+func (r Router) Endpoints() []string {
+	es := make([]string, 0, len(r.endpoints))
+	for e := range r.endpoints {
+		es = append(es, e)
 	}
-	sort.Strings(rs)
-	return rs
+	sort.Strings(es)
+	return es
 }
 
-func normalizeRoute(route string) string {
-	return strings.ToLower(strings.Trim(route, "/"))
+func normalizeEndpoint(endpoint string) string {
+	return strings.ToLower(strings.Trim(endpoint, "/"))
 }
