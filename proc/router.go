7bb5ff477a543104045253fd0cad23e3d38aca4a
Pasi Hyvönen <pasi@koud.fi>
2022-05-06 07:20:44 +0300

    Remove redundant router package, forgo "route parameters"

--- proc/router.go
+++ proc/router.go
@@ -3,25 +3,39 @@ package proc
 import (
 	"context"
 	"fmt"
-
-	"github.com/koud-fi/pkg/router"
+	"sort"
+	"strings"
 )
 
 type Router struct {
-	router.Router[Proc]
+	routeMap map[string]Proc
 }
 
-func NewRouter() *Router {
-	return &Router{Router: router.New[Proc]()}
+func NewRouter() Router {
+	return Router{routeMap: make(map[string]Proc)}
+}
+
+func (r *Router) Add(route string, pr Proc) {
+	r.routeMap[normalizeRoute(route)] = pr
 }
 
 func (r Router) Invoke(ctx context.Context, route string, p Params) (any, error) {
-	pr, _ := r.Lookup(route)
-	if !pr.IsValid() {
+	pr, ok := r.routeMap[normalizeRoute(route)]
+	if !ok {
 		return nil, fmt.Errorf("not found: %s", route)
 	}
-
-	// TODO: handle route params
-
 	return pr.Invoke(ctx, p)
+}
+
+func (r Router) Routes() []string {
+	rs := make([]string, 0, len(r.routeMap))
+	for r := range r.routeMap {
+		rs = append(rs, r)
+	}
+	sort.Strings(rs)
+	return rs
+}
+
+func normalizeRoute(route string) string {
+	return strings.ToLower(strings.Trim(route, "/"))
 }
