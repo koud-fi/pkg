bb08375731ff5e507a5089c2ac0b9c50b74c7e5a
Pasi Hyvönen <pasi@koud.fi>
2022-07-13 15:08:04 +0300

    Add TestNoise2D

--- noise/simplex/simplex_test.go
+++ noise/simplex/simplex_test.go
@@ -23,6 +23,19 @@ func TestNoise1D(t *testing.T) {
 	writeImageFile(t, "temp/simplex_1d.png", img)
 }
 
+func TestNoise2D(t *testing.T) {
+	img := image.NewGray(image.Rect(0, 0, 1024, 1024))
+	for x := 0; x < img.Rect.Dx(); x++ {
+		for y := 0; y < img.Rect.Dy(); y++ {
+			n := simplex.Noise2D(
+				float32(x)/float32(img.Rect.Dx()),
+				float32(y)/float32(img.Rect.Dy()))
+			img.SetGray(x, y, color.Gray{Y: uint8(n * 255)})
+		}
+	}
+	writeImageFile(t, "temp/simplex_2d.png", img)
+}
+
 func writeImageFile(t *testing.T, path string, img image.Image) {
 	buf := bytes.NewBuffer(nil)
 	if err := png.Encode(buf, img); err != nil {
