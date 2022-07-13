7422e472424d576a894742986f19e424ffce91f3
Pasi Hyvönen <pasi@koud.fi>
2022-07-13 18:02:05 +0300

    Play around with noise octaves

--- noise/simplex/simplex_test.go
+++ noise/simplex/simplex_test.go
@@ -24,12 +24,21 @@ func TestNoise1D(t *testing.T) {
 }
 
 func TestNoise2D(t *testing.T) {
-	img := image.NewGray(image.Rect(0, 0, 1024, 1024))
+	var (
+		img = image.NewGray(image.Rect(0, 0, 1024, 1024))
+		fdx = float32(img.Rect.Dx())
+		fdy = float32(img.Rect.Dy())
+	)
 	for x := 0; x < img.Rect.Dx(); x++ {
+		fx := float32(x) / fdx
 		for y := 0; y < img.Rect.Dy(); y++ {
-			n := simplex.Noise2D(
-				float32(x)/float32(img.Rect.Dx()/8),
-				float32(y)/float32(img.Rect.Dy()/8))
+			fy := float32(y) / fdy
+			n := simplex.Noise2D(fx, fy) +
+				0.5*simplex.Noise2D(2*fx, 2*fy) +
+				0.25*simplex.Noise2D(4*fx, 4*fy) +
+				0.1*simplex.Noise2D(8*fx, 8*fy) +
+				0.025*simplex.Noise2D(16*fx, 16*fy)
+			n /= 1.875
 			img.SetGray(x, y, color.Gray{Y: uint8((n + 1) * 127)})
 		}
 	}
