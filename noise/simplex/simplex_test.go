32607b7519c29d1b7c074db5a67c2b0a2c26b533
Pasi Hyvönen <pasi@koud.fi>
2022-07-13 15:05:31 +0300

    Write noise image from simplex test

--- noise/simplex/simplex_test.go
+++ noise/simplex/simplex_test.go
@@ -1,13 +1,37 @@
 package simplex_test
 
 import (
+	"bytes"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/koud-fi/pkg/noise/simplex"
 )
 
 func TestNoise1D(t *testing.T) {
-	for i := 0; i <= 20; i++ {
-		t.Log(i, simplex.Noise1D(float32(i)/20))
+	img := image.NewGray(image.Rect(0, 0, 1024, 32))
+	for x := 0; x < img.Rect.Dx(); x++ {
+		n := simplex.Noise1D(float32(x) / float32(img.Rect.Dx()))
+		for y := 0; y < img.Rect.Dy(); y++ {
+			img.SetGray(x, y, color.Gray{Y: uint8(n * 255)})
+		}
+	}
+	writeImageFile(t, "temp/simplex_1d.png", img)
+}
+
+func writeImageFile(t *testing.T, path string, img image.Image) {
+	buf := bytes.NewBuffer(nil)
+	if err := png.Encode(buf, img); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.MkdirAll(filepath.Dir(path), 0700); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.WriteFile(path, buf.Bytes(), 0600); err != nil {
+		t.Fatal(err)
 	}
 }
