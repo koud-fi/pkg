806f054f15febba75b65599bdb81cdee42eb2c97
Pasi Hyvönen <pasi@koud.fi>
2022-07-28 18:20:43 +0300

    Implement basic input parsing

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -2,10 +2,13 @@ package httpapi
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/koud-fi/pkg/blob"
 	"github.com/koud-fi/pkg/serve"
@@ -42,11 +45,16 @@ func New[T1, T2 any](fn func(context.Context, T1) (T2, error), opt ...serve.Opti
 
 func (e Endpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	serve.Handle(w, r, func() (*serve.Info, error) {
-		var args [2]reflect.Value
-		args[0] = reflect.ValueOf(r.Context())
-
-		// TODO: args[1]
-
+		args := [2]reflect.Value{
+			reflect.ValueOf(r.Context()),
+			reflect.New(e.inType),
+		}
+		if err := applyInput(args[1].Interface(), r); err != nil {
+			return nil, fmt.Errorf("can't apply params: %w", err)
+		}
+		if !e.inTypeIsPtr {
+			args[1] = args[1].Elem()
+		}
 		out := e.fn.Call(args[:])
 		if !out[1].IsNil() {
 			return nil, out[1].Interface().(error)
@@ -68,4 +76,103 @@ func (e Endpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return serve.JSON(w, r, v, e.opts...)
 		}
 	})
+}
+
+func applyInput(v any, r *http.Request) error {
+	ct := r.Header.Get("Content-Type")
+	switch {
+	case strings.HasPrefix("application/json", ct):
+		return json.NewDecoder(r.Body).Decode(v)
+
+	// TODO: support multipart forms
+
+	default:
+		if err := r.ParseForm(); err != nil {
+			return err
+		}
+		return applyValues(reflect.ValueOf(v), func(key string) []string {
+			return r.Form[key]
+		})
+	}
+}
+
+func applyValues(v reflect.Value, lookup func(key string) []string) error {
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	// TODO: non-struct value handling?
+
+	var (
+		vType = v.Type()
+		vLen  = v.NumField()
+		err   error
+	)
+	for i := 0; i < vLen && err == nil; i++ {
+		var (
+			f     = vType.Field(i)
+			fVal  = v.Field(i)
+			fName = resolveFieldName(f)
+		)
+		if f.Anonymous {
+			applyValues(fVal, lookup)
+			continue
+		}
+		vs := lookup(fName)
+		if len(vs) == 0 {
+			continue
+		}
+		if fVal.CanAddr() && fVal.CanInterface() {
+			vi := fVal.Addr().Interface()
+			if u, ok := vi.(json.Unmarshaler); ok {
+				err = u.UnmarshalJSON([]byte(vs[0]))
+				continue
+			}
+		}
+		fKind := f.Type.Kind()
+		switch fKind {
+		case reflect.String:
+			fVal.SetString(vs[0])
+
+		case reflect.Int, reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8:
+			var i int64
+			if i, err = strconv.ParseInt(vs[0], 10, f.Type.Bits()); err == nil {
+				fVal.SetInt(i)
+			}
+		case reflect.Uint, reflect.Uint64, reflect.Uint32, reflect.Uint16, reflect.Uint8:
+			var u uint64
+			if u, err = strconv.ParseUint(vs[0], 10, f.Type.Bits()); err == nil {
+				fVal.SetUint(u)
+			}
+		case reflect.Float64, reflect.Float32:
+			var fl float64
+			if fl, err = strconv.ParseFloat(vs[0], f.Type.Bits()); err == nil {
+				fVal.SetFloat(fl)
+			}
+		case reflect.Bool:
+			var b bool
+			if b, err = strconv.ParseBool(vs[0]); err == nil {
+				fVal.SetBool(b)
+			}
+
+		// TODO: slice handling
+
+		// TODO: struct handling
+
+		default:
+			return fmt.Errorf("unsupported kind %v for value: %s", fKind, fName)
+		}
+	}
+	return err
+}
+
+func resolveFieldName(f reflect.StructField) string {
+	name := f.Name
+	if tag, ok := f.Tag.Lookup("json"); ok {
+		parts := strings.SplitN(tag, ",", 2)
+		if parts[0] != "" {
+			name = parts[0]
+		}
+	}
+	return name
 }
--- endpoint/go.mod
+++ endpoint/go.mod
@@ -1,3 +1,13 @@
 module github.com/koud-fi/pkg/endpoint
 
 go 1.18
+
+require (
+	github.com/koud-fi/pkg/blob v0.0.0-20220728144657-c6f4d528b26a
+	github.com/koud-fi/pkg/serve v0.0.0-20220728144657-c6f4d528b26a
+)
+
+require (
+	github.com/koud-fi/pkg/rx v0.0.0-20220617062645-d7d71be010cb // indirect
+	golang.org/x/exp v0.0.0-20220414153411-bcd21879b8fd // indirect
+)
