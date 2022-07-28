94f4bf930387142e6cc60e5e0e43c143c05a165e
Pasi Hyvönen <pasi@koud.fi>
2022-07-28 18:32:18 +0300

    Use merge package

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -1,4 +1,4 @@
-package httpapi
+package endpoint
 
 import (
 	"context"
@@ -7,10 +7,10 @@ import (
 	"io"
 	"net/http"
 	"reflect"
-	"strconv"
 	"strings"
 
 	"github.com/koud-fi/pkg/blob"
+	"github.com/koud-fi/pkg/merge"
 	"github.com/koud-fi/pkg/serve"
 )
 
@@ -90,86 +90,8 @@ func applyInput(v any, r *http.Request) error {
 		if err := r.ParseForm(); err != nil {
 			return err
 		}
-		return applyValues(reflect.ValueOf(v), func(key string) []string {
+		return merge.Values(v, func(key string) []string {
 			return r.Form[key]
 		})
 	}
-}
-
-func applyValues(v reflect.Value, lookup func(key string) []string) error {
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
-	var (
-		vType = v.Type()
-		vLen  = v.NumField()
-		err   error
-	)
-	for i := 0; i < vLen && err == nil; i++ {
-		var (
-			f     = vType.Field(i)
-			fVal  = v.Field(i)
-			fName = resolveFieldName(f)
-		)
-		if f.Anonymous {
-			applyValues(fVal, lookup)
-			continue
-		}
-		vs := lookup(fName)
-		if len(vs) == 0 {
-			continue
-		}
-		if fVal.CanAddr() && fVal.CanInterface() {
-			vi := fVal.Addr().Interface()
-			if u, ok := vi.(json.Unmarshaler); ok {
-				err = u.UnmarshalJSON([]byte(vs[0]))
-				continue
-			}
-		}
-		fKind := f.Type.Kind()
-		switch fKind {
-		case reflect.String:
-			fVal.SetString(vs[0])
-
-		case reflect.Int, reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8:
-			var i int64
-			if i, err = strconv.ParseInt(vs[0], 10, f.Type.Bits()); err == nil {
-				fVal.SetInt(i)
-			}
-		case reflect.Uint, reflect.Uint64, reflect.Uint32, reflect.Uint16, reflect.Uint8:
-			var u uint64
-			if u, err = strconv.ParseUint(vs[0], 10, f.Type.Bits()); err == nil {
-				fVal.SetUint(u)
-			}
-		case reflect.Float64, reflect.Float32:
-			var fl float64
-			if fl, err = strconv.ParseFloat(vs[0], f.Type.Bits()); err == nil {
-				fVal.SetFloat(fl)
-			}
-		case reflect.Bool:
-			var b bool
-			if b, err = strconv.ParseBool(vs[0]); err == nil {
-				fVal.SetBool(b)
-			}
-
-		// TODO: slice handling
-
-		// TODO: struct handling
-
-		default:
-			return fmt.Errorf("unsupported kind %v for value: %s", fKind, fName)
-		}
-	}
-	return err
-}
-
-func resolveFieldName(f reflect.StructField) string {
-	name := f.Name
-	if tag, ok := f.Tag.Lookup("json"); ok {
-		parts := strings.SplitN(tag, ",", 2)
-		if parts[0] != "" {
-			name = parts[0]
-		}
-	}
-	return name
 }
