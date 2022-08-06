e56039780a5a8d8a47d9aa22ff0d31f518a08856
koud-fi <software@koud.fi>
2022-08-06 10:18:06 +0300

    Fix rx.Random; add type parameter

--- rx/random.go
+++ rx/random.go
@@ -1,7 +1,13 @@
 package rx
 
-func Random(min, max int) Iter[N] {
+import (
+	"math/rand"
+
+	"golang.org/x/exp/constraints"
+)
+
+func Random[N constraints.Integer](min, max N) Iter[N] {
 	return FuncIter(func() ([]N, bool, error) {
-		return []N{rand.Intn(max - min) + min}, true, nil
+		return []N{N(rand.Int63n(int64(max)-int64(min))) + min}, true, nil
 	})
 }
