60fa195b83752193a75c597276263bee29e4dbb7
Pasi Hyvönen <pasi@koud.fi>
2022-08-13 07:45:42 +0300

    Add Slicer interface to optimize slice handling

--- rx/slice.go
+++ rx/slice.go
@@ -1,5 +1,9 @@
 package rx
 
+type Slicer[T any] interface {
+	Slice() []T
+}
+
 func SliceIter[T any](data ...T) Iter[T] {
 	return &sliceIter[T]{data: data}
 }
@@ -20,7 +24,11 @@ func (it *sliceIter[_]) Next() bool {
 
 func (it sliceIter[T]) Value() T     { return it.data[0] }
 func (it sliceIter[_]) Close() error { return nil }
+func (it sliceIter[T]) Slice() []T
 
 func Slice[T any](it Iter[T]) ([]T, error) {
+	if s, ok := it.(Slicer[T]); ok {
+		return s.Slice(), nil
+	}
 	return Reduce(it, func(s []T, v T) ([]T, error) { return append(s, v), nil }, []T{})
 }
