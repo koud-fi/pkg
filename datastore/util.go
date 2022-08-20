94defc9778f3482354350197706223a719855d9e
Pasi Hyvönen <pasi@koud.fi>
2022-08-20 13:16:17 +0300

    Add datastore.Sync utility function

---
+++ datastore/util.go
@@ -0,0 +1,13 @@
+package datastore
+
+import (
+	"context"
+
+	"github.com/koud-fi/pkg/rx"
+)
+
+func Sync[T any](ctx context.Context, dst, src *Store[T], after string) error {
+	return rx.ForEach(src.Iter(ctx, after), func(p rx.Pair[string, T]) error {
+		return dst.Set(ctx, p.Key, p.Value)
+	})
+}
