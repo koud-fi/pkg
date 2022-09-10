8a0e3f9e321ee42ae695d7caebe1233a0d3a9a40
Pasi Hyvönen <pasi@koud.fi>
2022-09-10 14:09:04 +0300

    Refactor codec creation, add Strings and Bytes codecs

--- datastore/codec.go
+++ datastore/codec.go
@@ -11,14 +11,27 @@ type Codec[T any] interface {
 	Unmarshal(blob.Blob) (T, error)
 }
 
-func JSON[T any]() Codec[T] { return jsonCodec[T]{} }
+var _ Codec[any] = (*Funcs[any])(nil)
 
-type jsonCodec[T any] struct{}
-
-func (jsonCodec[T]) Marshal(v T) blob.Blob {
-	return blob.Marshal(json.Marshal, v)
+type Funcs[T any] struct {
+	M func(T) blob.Blob
+	U func(blob.Blob) (T, error)
 }
 
-func (jsonCodec[T]) Unmarshal(b blob.Blob) (v T, _ error) {
-	return v, blob.Unmarshal(json.Unmarshal, b, &v)
+func (c Funcs[T]) Marshal(v T) blob.Blob            { return c.M(v) }
+func (c Funcs[T]) Unmarshal(b blob.Blob) (T, error) { return c.U(b) }
+
+func JSON[T any]() Codec[T] {
+	return Funcs[T]{
+		M: func(v T) blob.Blob { return blob.Marshal(json.Marshal, v) },
+		U: func(b blob.Blob) (v T, _ error) { return v, blob.Unmarshal(json.Unmarshal, b, &v) },
+	}
+}
+
+func Strings() Codec[string] {
+	return Funcs[string]{M: blob.FromString, U: blob.String}
+}
+
+func Bytes() Codec[[]byte] {
+	return Funcs[[]byte]{M: blob.FromBytes, U: blob.Bytes}
 }
