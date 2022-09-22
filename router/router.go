eb10ea80fd5703c550645e2ab8fdcb43607115c0
Pasi Hyvönen <pasi@koud.fi>
2022-09-22 07:48:17 +0300

    Add router.node type

--- router/router.go
+++ router/router.go
@@ -1,19 +1,29 @@
 package router
 
 type Router[T any] struct {
-	// TODO
+	tree map[string]node[T]
 }
 
-func (r Router[T]) Register(method, path string, v T) {
+func New[T any]() *Router[T] {
+	return &Router[T]{tree: make(map[string]node[T])}
+}
+
+func (r *Router[T]) Register(method, path string, v T) {
 
 	// ???
 
 	panic("TODO")
 }
 
-func (r Router[T]) Lookup(method, path string) (T, func(string) string, bool) {
+func (r *Router[T]) Lookup(method, path string) (T, func(string) string, bool) {
 
 	// ???
 
 	panic("TODO")
+}
+
+type node[T any] struct {
+	node    map[string]node[T]
+	isParam bool
+	value   T
 }
