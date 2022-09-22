356e5116b392a93f70aae1bf9bf4848f7e96c9d4
Pasi Hyvönen <pasi@koud.fi>
2022-09-22 07:30:16 +0300

    Changes to router API

--- router/router.go
+++ router/router.go
@@ -1,16 +1,17 @@
 package router
 
-import "strings"
-
 type Router[T any] struct {
 	// TODO
 }
 
-func (r Router[T]) Lookup(path ...string) (T, func(string) string, bool) {
-	frags := make([]string, 0, len(path))
-	for _, p := range path {
-		frags = append(frags, strings.Split(p, "/")...)
-	}
+func (r Router[T]) Register(method, path string, v T) {
+
+	// ???
+
+	panic("TODO")
+}
+
+func (r Router[T]) Lookup(method, path string) (T, func(string) string, bool) {
 
 	// ???
 
