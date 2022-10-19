7dda0b6bbc508acc0c64e53ab8c2cb831cfdcb8c
Pasi Hyvönen <pasi@koud.fi>
2022-10-19 06:48:00 +0300

    Add more API types

--- schema/api.go
+++ schema/api.go
@@ -18,18 +18,33 @@ type Server struct {
 }
 
 type Path struct {
-	Summary     string    `json:"summary,omitempty"`
-	Description string    `json:"description,omitempty"`
-	Get         Operation `json:"get,omitempty"`
-	Put         Operation `json:"put,omitempty"`
-	Post        Operation `json:"post,omitempty"`
-	Delete      Operation `json:"delete,omitempty"`
-	Options     Operation `json:"options,omitempty"`
-	Head        Operation `json:"head,omitempty"`
-	Patch       Operation `json:"patch,omitempty"`
-	Trace       Operation `json:"trace,omitempty"`
+	Summary     string     `json:"summary,omitempty"`
+	Description string     `json:"description,omitempty"`
+	Get         *Operation `json:"get,omitempty"`
+	Put         *Operation `json:"put,omitempty"`
+	Post        *Operation `json:"post,omitempty"`
+	Delete      *Operation `json:"delete,omitempty"`
+	Options     *Operation `json:"options,omitempty"`
+	Head        *Operation `json:"head,omitempty"`
+	Patch       *Operation `json:"patch,omitempty"`
+	Trace       *Operation `json:"trace,omitempty"`
 }
 
 type Operation struct {
+	Parameters []Parameter         `json:"parameters,omitempty"`
+	Response   map[string]Response `json:"response"`
+}
+
+type Parameter struct {
 	// TODO
+}
+
+type Response struct {
+	Description string               `json:"description,omitempty"`
+	Content     map[string]MediaType `json:"content,omitempty"`
+}
+
+type MediaType struct {
+	Description string  `json:"description,omitempty"`
+	Schema      *Schema `json:"schema,omitempty"`
 }
