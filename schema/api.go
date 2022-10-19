f2540095adbddf09f237b424a54e0d82120f089f
Pasi Hyvönen <pasi@koud.fi>
2022-10-19 06:29:34 +0300

    Add basic API schema type

---
+++ schema/api.go
@@ -0,0 +1,35 @@
+package schema
+
+type API struct {
+	Info    Info            `json:"info"`
+	Servers []Server        `json:"servers"`
+	Paths   map[string]Path `json:"paths"`
+}
+
+type Info struct {
+	Title       string `json:"title"`
+	Description string `json:"description,omitempty"`
+	Version     string `json:"version"`
+}
+
+type Server struct {
+	URL         string `json:"url"`
+	Description string `json:"description,omitempty"`
+}
+
+type Path struct {
+	Summary     string    `json:"summary,omitempty"`
+	Description string    `json:"description,omitempty"`
+	Get         Operation `json:"get,omitempty"`
+	Put         Operation `json:"put,omitempty"`
+	Post        Operation `json:"post,omitempty"`
+	Delete      Operation `json:"delete,omitempty"`
+	Options     Operation `json:"options,omitempty"`
+	Head        Operation `json:"head,omitempty"`
+	Patch       Operation `json:"patch,omitempty"`
+	Trace       Operation `json:"trace,omitempty"`
+}
+
+type Operation struct {
+	// TODO
+}
