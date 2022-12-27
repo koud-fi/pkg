b8e50d7389067a126e52a80c040bde32b5e02221
Pasi Hyvönen <pasi@koud.fi>
2022-12-27 13:24:35 +0200

    Simplify Node interface

--- rq/node.go
+++ rq/node.go
@@ -3,7 +3,17 @@ package rq
 import "github.com/koud-fi/pkg/rx"
 
 type Node interface {
-	Tag() string
+	Attrs(keys ...string) rx.Iter[rx.Pair[string, any]]
+}
+
+type AttrNode interface {
 	Attr(key string) any
-	Children(tag ...string) rx.Iter[Node]
+}
+
+func Attr(n Node, key string) any {
+	v, _, err := rx.First(n.Attrs(key))
+	if err != nil {
+		return err
+	}
+	return v
 }
