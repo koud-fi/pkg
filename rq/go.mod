8b38a6adbcc87d96c4c7ac260b54a7f159bc13e2
Pasi Hyvönen <pasi@koud.fi>
2022-12-27 13:53:40 +0200

    Improve map test

--- rq/go.mod
+++ rq/go.mod
@@ -2,6 +2,9 @@ module github.com/koud-fi/pkg/rq
 
 go 1.18
 
-require github.com/koud-fi/pkg/rx v0.0.0-20221217060217-20480eb80a99
+require (
+	github.com/koud-fi/pkg/blob v0.0.0-20221227113902-1dba4a42853d
+	github.com/koud-fi/pkg/rx v0.0.0-20221217060217-20480eb80a99
+)
 
 require golang.org/x/exp v0.0.0-20220414153411-bcd21879b8fd // indirect
--- rq/map_test.go
+++ rq/map_test.go
@@ -1,12 +1,20 @@
 package rq_test
 
 import (
+	"encoding/json"
 	"testing"
 
+	"github.com/koud-fi/pkg/blob"
+	"github.com/koud-fi/pkg/blob/localfile"
 	"github.com/koud-fi/pkg/rq"
 )
 
 func TestMap(t *testing.T) {
-	n := rq.MapNode[any](map[string]any{"a": 42})
+	var data map[string]any
+	if err := blob.Unmarshal(json.Unmarshal, localfile.New("testdata.json"), &data); err != nil {
+		t.Fatal(err)
+	}
+	n := rq.MapNode[any](data)
 	t.Log(rq.Attr(n, "a"))
+	t.Log(rq.Attr(n, "b"))
 }
