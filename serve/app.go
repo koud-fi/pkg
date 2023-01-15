10b9b8021e44d95fc794b7757ebb430ba32ce83f
Pasi Hyvönen <pasi@koud.fi>
2023-01-15 10:35:54 +0200

    Improve serve.App

--- serve/app.go
+++ serve/app.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	pathpkg "path"
+	"strings"
 
 	"github.com/koud-fi/pkg/blob"
 )
@@ -15,11 +16,16 @@ const DefaultIndexFile = "index.html"
 
 type appConfig struct {
 	index string
+	root  string
 }
 
 type AppOption func(*appConfig)
 
 func Index(name string) AppOption { return func(c *appConfig) { c.index = name } }
+
+func Root(dir string) AppOption {
+	return func(c *appConfig) { c.root = strings.TrimSuffix(dir, "/") + "/" }
+}
 
 func App(fsys fs.FS, opt ...AppOption) http.Handler {
 	c := appConfig{
@@ -33,21 +39,22 @@ func App(fsys fs.FS, opt ...AppOption) http.Handler {
 			if r.Method != http.MethodGet {
 				return nil, os.ErrInvalid
 			}
-			path := r.URL.Path
+			path := c.root + r.URL.Path
 			info, err := fs.Stat(fsys, path)
 			if err != nil {
 				if !os.IsNotExist(errors.Unwrap(err)) {
 					return nil, err
 				}
 			}
-			var opts []Option
 			switch {
-			case !info.IsDir():
-				if ext := pathpkg.Ext(path); ext != "" {
-					opts = append(opts, ContentType(mime.TypeByExtension(ext)))
-				}
+			case info != nil && !info.IsDir():
+				break
 			default:
-				path = c.index
+				path = c.root + c.index
+			}
+			var opts []Option
+			if ext := pathpkg.Ext(path); ext != "" {
+				opts = append(opts, ContentType(mime.TypeByExtension(ext)))
 			}
 			return Blob(w, r, blob.FromFS(fsys, path), opts...)
 		})
