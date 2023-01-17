2243f39e496e1eee2bc0d8ecac4522ba82cae44d
Pasi Hyvönen <pasi@koud.fi>
2023-01-17 11:10:34 +0200

    Add Partial Maybe method

--- rx/maybe.go
+++ rx/maybe.go
@@ -5,8 +5,9 @@ type Maybe[T any] struct {
 	ok bool
 }
 
-func Just[T any](v T) Maybe[T] { return Maybe[T]{v: v, ok: true} }
-func None[T any]() Maybe[T]    { return Maybe[T]{} }
+func Just[T any](v T) Maybe[T]    { return Maybe[T]{v: v, ok: true} }
+func Partial[T any](v T) Maybe[T] { return Maybe[T]{v: v} }
+func None[T any]() Maybe[T]       { return Maybe[T]{} }
 
 func (m Maybe[T]) Value() T { return m.v }
 func (m Maybe[_]) Ok() bool { return m.ok }
