d2b2223d6fddf962c3033df7530f0f2378e7c3c8
Pasi Hyvönen <pasi@koud.fi>
2023-01-17 18:06:37 +0200

    Remove pointless comparable constraint from pair key

--- rx/pair.go
+++ rx/pair.go
@@ -2,23 +2,23 @@ package rx
 
 import "golang.org/x/exp/constraints"
 
-type Pair[K comparable, V any] struct {
+type Pair[K, V any] struct {
 	key   K
 	value V
 }
 
-func NewPair[K comparable, V any](key K, value V) Pair[K, V] {
+func NewPair[K, V any](key K, value V) Pair[K, V] {
 	return Pair[K, V]{key, value}
 }
 
 func (p Pair[K, _]) Key() K   { return p.key }
 func (p Pair[_, V]) Value() V { return p.value }
 
-func Pluck[K comparable, V any](it Iter[V], keyFn func(V) K) Iter[Pair[K, V]] {
+func Pluck[K, V any](it Iter[V], keyFn func(V) K) Iter[Pair[K, V]] {
 	return PluckErr(it, func(v V) (K, error) { return keyFn(v), nil })
 }
 
-func PluckErr[K comparable, V any](it Iter[V], keyFn func(V) (K, error)) Iter[Pair[K, V]] {
+func PluckErr[K, V any](it Iter[V], keyFn func(V) (K, error)) Iter[Pair[K, V]] {
 	return MapErr(it, func(v V) (p Pair[K, V], err error) {
 		p.value = v
 		p.key, err = keyFn(v)
