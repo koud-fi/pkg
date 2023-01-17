276e822fc105ccd510f155f1d59790ec6945ec4c
Pasi Hyvönen <pasi@koud.fi>
2023-01-17 13:37:14 +0200

    Add rx.IsOk filtering helper function

--- rx/maybe.go
+++ rx/maybe.go
@@ -11,3 +11,5 @@ func None[T any]() Maybe[T]       { return Maybe[T]{} }
 
 func (m Maybe[T]) Value() T { return m.v }
 func (m Maybe[_]) Ok() bool { return m.ok }
+
+func IsOk[T any](v Maybe[T]) bool { return v.Ok() }
