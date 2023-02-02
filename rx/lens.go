b6ed841b9b045884dd7c53b5c66e4212419b665d
Pasi Hyvönen <pasi@koud.fi>
2023-02-02 22:26:59 +0200

    Add valueLens type

--- rx/lens.go
+++ rx/lens.go
@@ -5,4 +5,9 @@ type Lens[T any] interface {
 	Set(T) error
 }
 
-// TODO: ???
+type valueLens[T any] struct{ v T }
+
+func (vl valueLens[T]) Get() (T, error) { return vl.v, nil }
+func (vl *valueLens[T]) Set(v T) error  { vl.v = v; return nil }
+
+func Value[T any](v T) Lens[T] { return &valueLens[T]{v} }
