84d44a6415d1a31c05e6c18e122fdf1c8f85936d
Pasi Hyvönen <pasi@koud.fi>
2023-02-03 07:03:18 +0200

    Pass context to lens functions

--- rx/lens.go
+++ rx/lens.go
@@ -1,16 +1,20 @@
 package rx
 
-import "sync"
+import (
+	"context"
+	"errors"
+	"sync"
+)
 
 type Lens[T any] interface {
-	Get() (T, error)
-	Set(T) error
+	Get(context.Context) (T, error)
+	Set(context.Context, T) error
 }
 
 type valueLens[T any] struct{ v T }
 
-func (vl valueLens[T]) Get() (T, error) { return vl.v, nil }
-func (vl *valueLens[T]) Set(v T) error  { vl.v = v; return nil }
+func (vl valueLens[T]) Get(context.Context) (T, error)    { return vl.v, nil }
+func (vl *valueLens[T]) Set(_ context.Context, v T) error { vl.v = v; return nil }
 
 func Value[T any](v T) Lens[T] { return &valueLens[T]{v} }
 
@@ -19,38 +23,42 @@ type atomicLens[T any] struct {
 	mu sync.RWMutex
 }
 
-func (al *atomicLens[T]) Get() (T, error) {
+func (al *atomicLens[T]) Get(ctx context.Context) (T, error) {
 	al.mu.RLock()
 	defer al.mu.RUnlock()
-	return al.l.Get()
+	return al.l.Get(ctx)
 }
 
-func (al *atomicLens[T]) Set(v T) error {
+func (al *atomicLens[T]) Set(ctx context.Context, v T) error {
 	al.mu.Lock()
 	defer al.mu.Unlock()
-	return al.l.Set(v)
+	return al.l.Set(ctx, v)
 }
 
 func Atomic[T any](l Lens[T]) Lens[T] { return &atomicLens[T]{l: l} }
 
 type onceLens[T any] struct {
-	fn   func() (T, error)
+	fn   func(context.Context) (T, error)
 	init bool
 	v    T
 	err  error
 }
 
-func Once[T any](fn func() (T, error)) Lens[T] { return &onceLens[T]{fn: fn} }
+func Once[T any](fn func(context.Context) (T, error)) Lens[T] { return &onceLens[T]{fn: fn} }
 
-func (ol *onceLens[T]) Get() (v T, _ error) {
+func (ol *onceLens[T]) Get(ctx context.Context) (T, error) {
 	if !ol.init {
-		ol.v, ol.err = ol.fn()
-		ol.init = true
+		v, err := ol.fn(ctx)
+		if err != nil &&
+			(errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded)) {
+			return v, err
+		}
+		ol.init, ol.v, ol.err = true, v, err
 	}
 	return ol.v, ol.err
 }
 
-func (ol *onceLens[T]) Set(v T) error {
-	ol.init, ol.v, ol.err = false, v, nil
+func (ol *onceLens[T]) Set(_ context.Context, v T) error {
+	ol.init, ol.v, ol.err = true, v, nil
 	return nil
 }
