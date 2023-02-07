d056944c9d1e602605465a461b31d620a77189dc
Pasi Hyvönen <pasi@koud.fi>
2023-02-07 18:23:32 +0200

    Add FSGetter helper constructor

--- blob/storage.go
+++ blob/storage.go
@@ -3,6 +3,8 @@ package blob
 import (
 	"context"
 	"io"
+	"io/fs"
+	"path"
 
 	"github.com/koud-fi/pkg/rx"
 )
@@ -22,10 +24,6 @@ type Getter interface {
 	Get(ctx context.Context, ref string) Blob
 }
 
-type GetterFunc func(ctx context.Context, ref string) Blob
-
-func (f GetterFunc) Get(ctx context.Context, ref string) Blob { return f(ctx, ref) }
-
 type Setter interface {
 	Set(ctx context.Context, ref string, r io.Reader) error
 }
@@ -41,4 +39,14 @@ type RefBlob struct {
 
 type Iterator interface {
 	Iter(ctx context.Context, after string) rx.Iter[RefBlob]
+}
+
+type GetterFunc func(ctx context.Context, ref string) Blob
+
+func (f GetterFunc) Get(ctx context.Context, ref string) Blob { return f(ctx, ref) }
+
+func FSGetter(fsys fs.FS) Getter {
+	return GetterFunc(func(_ context.Context, ref string) Blob {
+		return FromFS(fsys, path.Clean(ref))
+	})
 }
