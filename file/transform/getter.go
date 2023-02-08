fde557bb1babf54caeef9259f35caef90a7608a9
Pasi Hyvönen <pasi@koud.fi>
2023-02-08 08:51:36 +0200

    Implement ImageGetter

--- file/transform/getter.go
+++ file/transform/getter.go
@@ -1,10 +1,26 @@
 package transform
 
-import "github.com/koud-fi/pkg/blob"
+import (
+	"context"
+	"io"
+
+	"github.com/koud-fi/pkg/blob"
+)
 
 const (
 	Image blob.Domain = "image"
 	//Video blob.Domain = "video"
 )
 
-// TODO
+func ImageGetter(g blob.Getter) blob.Getter {
+	return blob.GetterFunc(func(ctx context.Context, ref blob.Ref) blob.Blob {
+		return blob.Func(func() (io.ReadCloser, error) {
+			ref = ref.Ref()
+			p, err := ParseParams(string(ref.Domain()))
+			if err != nil {
+				return nil, err
+			}
+			return ToImage(g.Get(ctx, ref.Ref()), p).Open()
+		})
+	})
+}
