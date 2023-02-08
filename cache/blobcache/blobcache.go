ad2f310ff88d298ea230309d4d32b00f36a6607b
Pasi Hyvönen <pasi@koud.fi>
2023-02-08 06:55:16 +0200

    Fix compiler errors in blob cache

--- cache/blobcache/blobcache.go
+++ cache/blobcache/blobcache.go
@@ -1,13 +1,21 @@
 package blobcache
 
 import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"errors"
+	"io"
+	"os"
+
+	"github.com/koud-fi/pkg/blob"
+	"github.com/koud-fi/pkg/cache"
+	"github.com/koud-fi/pkg/rx"
 )
 
 //lint:ignore ST1012 if returned from resolve; data is not written to storage.
 var NoCache = errors.New("no cache")
 
-/*
 type Cache struct {
 	*cache.Cache
 	backend
@@ -23,6 +31,7 @@ func (c *Cache) Resolve(ctx context.Context, key string, b blob.Blob) blob.Blob 
 		var (
 			digest = sha256.Sum256([]byte(key))
 			key    = hex.EncodeToString(digest[:])
+			keyRef = blob.NewRef(key)
 			out    io.ReadCloser
 		)
 		if err := c.Cache.Resolve(ctx, key, func() (int64, error) {
@@ -38,7 +47,7 @@ func (c *Cache) Resolve(ctx context.Context, key string, b blob.Blob) blob.Blob 
 
 			// TODO: resolve size correctly
 
-			return 0, c.s.Set(ctx, key, rc)
+			return 0, c.s.Set(ctx, keyRef, rc)
 
 		}); err != nil {
 			return nil, err
@@ -46,7 +55,7 @@ func (c *Cache) Resolve(ctx context.Context, key string, b blob.Blob) blob.Blob 
 		if out != nil {
 			return out, nil
 		}
-		return c.s.Get(ctx, key).Open()
+		return c.s.Get(ctx, keyRef).Open()
 	})
 }
 
@@ -55,7 +64,7 @@ type backend struct {
 }
 
 func (b *backend) Has(ctx context.Context, key string) (bool, error) {
-	if err := blob.Error(b.s.Get(ctx, key)); err == nil {
+	if err := blob.Error(b.s.Get(ctx, blob.NewRef(key))); err == nil {
 		return true, nil
 	} else if !os.IsNotExist(err) {
 		return false, err
@@ -64,12 +73,11 @@ func (b *backend) Has(ctx context.Context, key string) (bool, error) {
 }
 
 func (b *backend) Delete(ctx context.Context, key string) error {
-	return b.s.Delete(ctx, key)
+	return b.s.Delete(ctx, blob.NewRef(key))
 }
 
 func (b *backend) Keys(ctx context.Context) rx.Iter[rx.Pair[string, int64]] {
-	return rx.Map(b.s.Iter(ctx, ""), func(br blob.RefBlob) rx.Pair[string, int64] {
-		return rx.NewPair(br.Ref, int64(0)) // TODO: resolve blob sizes
+	return rx.Map(b.s.Iter(ctx, blob.Ref{}), func(br blob.RefBlob) rx.Pair[string, int64] {
+		return rx.NewPair(br.Ref.String(), int64(0)) // TODO: resolve blob sizes
 	})
 }
-*/
