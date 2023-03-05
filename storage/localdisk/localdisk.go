a6b6308e454988f5fc129437c1919035329e4aed
Pasi Hyvönen <pasi@koud.fi>
2023-03-05 17:32:12 +0200

    Move localdisk storage options to separate file

--- storage/localdisk/localdisk.go
+++ storage/localdisk/localdisk.go
@@ -20,22 +20,6 @@ const (
 	defaultFilePerm = os.FileMode(0600)
 )
 
-type Option func(*Storage)
-
-func Buckets(levels ...int) Option {
-	return func(s *Storage) {
-		s.bucketLevels = levels
-		s.bucketPrefixLen = 0
-		for _, l := range levels {
-			s.bucketPrefixLen += l + 1
-		}
-	}
-}
-
-func BucketHash(h crypto.Hash) Option { return func(s *Storage) { s.bucketHash = &h } }
-func DirPerm(m os.FileMode) Option    { return func(s *Storage) { s.dirPerm = m } }
-func FilePerm(m os.FileMode) Option   { return func(s *Storage) { s.filePerm = m } }
-
 var _ blob.SortedStorage = (*Storage)(nil)
 
 type Storage struct {
