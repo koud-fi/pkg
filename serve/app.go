9cb35a293527d4fd12784a5e06efd1484abe1dda
Pasi Hyvönen <pasi@koud.fi>
2023-03-07 20:19:41 +0200

    Overhaul app serving to be more extensible

--- serve/app.go
+++ serve/app.go
@@ -21,36 +21,40 @@ type AppOption func(*appConfig)
 
 func Index(name string) AppOption { return func(c *appConfig) { c.index = name } }
 
-func App(fsys fs.FS, opt ...AppOption) http.Handler {
+func App(opt ...AppOption) func(http.ResponseWriter, *http.Request, fs.FS, ...Option) (*Info, error) {
 	c := appConfig{
 		index: DefaultIndexFile,
 	}
 	for _, opt := range opt {
 		opt(&c)
 	}
+	return func(w http.ResponseWriter, r *http.Request, fsys fs.FS, opt ...Option) (*Info, error) {
+		if r.Method != http.MethodGet {
+			return nil, os.ErrInvalid
+		}
+		path := r.URL.Path
+		info, err := fs.Stat(fsys, path)
+		if err != nil {
+			if !os.IsNotExist(errors.Unwrap(err)) {
+				return nil, err
+			}
+		}
+		switch {
+		case info != nil && !info.IsDir():
+			break
+		default:
+			path = c.index
+		}
+		if ext := pathpkg.Ext(path); ext != "" {
+			opt = append(opt, ContentType(mime.TypeByExtension(ext)))
+		}
+		return Blob(w, r, blob.FromFS(fsys, path), opt...)
+	}
+}
+
+func HandleApp(fsys fs.FS, opt ...AppOption) http.Handler {
+	fn := App(opt...)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		Handle(w, r, func() (*Info, error) {
-			if r.Method != http.MethodGet {
-				return nil, os.ErrInvalid
-			}
-			path := r.URL.Path
-			info, err := fs.Stat(fsys, path)
-			if err != nil {
-				if !os.IsNotExist(errors.Unwrap(err)) {
-					return nil, err
-				}
-			}
-			switch {
-			case info != nil && !info.IsDir():
-				break
-			default:
-				path = c.index
-			}
-			var opts []Option
-			if ext := pathpkg.Ext(path); ext != "" {
-				opts = append(opts, ContentType(mime.TypeByExtension(ext)))
-			}
-			return Blob(w, r, blob.FromFS(fsys, path), opts...)
-		})
+		Handle(w, r, func() (*Info, error) { return fn(w, r, fsys) })
 	})
 }
