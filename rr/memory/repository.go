c544fccc7cb76d737bcd9b26b32c7138664ba948
Pasi Hyvönen <pasi@koud.fi>
2023-03-30 20:19:27 +0300

    Finalize memory repository

--- rr/memory/repository.go
+++ rr/memory/repository.go
@@ -10,92 +10,85 @@ import (
 )
 
 type memRW struct {
-	mu     sync.RWMutex
-	tables map[rr.Repository]*repoTable
+	mu    sync.RWMutex
+	repos map[rr.Repository]*memRepo
 }
 
 type KeyFunc func(rr.Item) string
 
-func NewRepository(tables map[rr.Repository]KeyFunc) rr.Writer {
-	r := memRW{
-		tables: make(map[rr.Repository]*repoTable, len(tables)),
+func NewRW(repos map[rr.Repository]KeyFunc) rr.Writer {
+	m := memRW{
+		repos: make(map[rr.Repository]*memRepo, len(repos)),
 	}
-	for t, keyFn := range tables {
-		r.tables[t] = &repoTable{
-			t:     t,
+	for r, keyFn := range repos {
+		m.repos[r] = &memRepo{
+			r:     r,
 			keyFn: keyFn,
 			data:  make(map[string][]byte),
 		}
 	}
-	return &r
+	return &m
 }
 
-/*
-func (r *memRepo) Get(ctx context.Context, in rr.GetInput) (rr.GetOutput, error) {
-	tables, err := resolveTables(r.tables, in)
+func (m *memRW) Read() rr.ReadTx {
+	return &readTx{memRW: m}
+}
+
+type readTx struct {
+	req map[rr.Repository][]rr.Key
+	*memRW
+}
+
+func (tx *readTx) Get(r rr.Repository, key rr.Key) {
+	tx.req[r] = append(tx.req[r], key)
+}
+
+func (tx *readTx) Execute(ctx context.Context) (map[rr.Repository][]rr.Item, error) {
+	repos, err := resolveRepos(tx.repos, tx.req)
 	if err != nil {
 		return nil, err
 	}
-	r.mu.RLock()
-	defer r.mu.RUnlock()
+	tx.mu.RLock()
+	defer tx.mu.RUnlock()
 
-	out := make(rr.GetOutput, len(tables))
-	for table, keys := range tables {
+	out := make(map[rr.Repository][]rr.Item, len(repos))
+	for repo, keys := range repos {
 		items := make([]rr.Item, 0, len(keys))
 		for _, k := range keys {
-			if item := table.get(table.keyFn(k)); item != nil {
+			if item := repo.get(repo.keyFn(k)); item != nil {
 				items = append(items, item)
 			}
 		}
-		out[table.t] = items
+		out[repo.r] = items
 	}
 	return out, nil
 }
-*/
 
-func (r *memRW) Write() rr.WriteTx {
-	return &writeTx{memRW: r, ops: make(map[rr.Repository][]func(*repoTable))}
-}
-
-type repoTable struct {
-	t     rr.Repository
-	keyFn KeyFunc
-	data  map[string][]byte
-}
-
-func (rt *repoTable) get(key string) (item rr.Item) {
-	if data, ok := rt.data[key]; ok {
-		json.Unmarshal(data, &item) // TODO: don't ignore error
-	}
-	return
-}
-
-func (rt *repoTable) put(key string, item rr.Item) {
-	data, _ := json.Marshal(item) // TODO: don't ignore error
-	rt.data[key] = data
+func (m *memRW) Write() rr.WriteTx {
+	return &writeTx{memRW: m, ops: make(map[rr.Repository][]func(*memRepo))}
 }
 
 type writeTx struct {
 	*memRW
-	ops map[rr.Repository][]func(*repoTable)
+	ops map[rr.Repository][]func(*memRepo)
 }
 
-func (tx *writeTx) Put(t rr.Repository, item rr.Item) {
-	tx.addOp(t, func(table *repoTable) {
-		table.put(table.keyFn(item), item)
+func (tx *writeTx) Put(r rr.Repository, item rr.Item) {
+	tx.addOp(r, func(repo *memRepo) {
+		repo.put(repo.keyFn(item), item)
 	})
 }
 
-func (tx *writeTx) Update(t rr.Repository, key rr.Key, update rr.Update) {
-	tx.addOp(t, func(table *repoTable) {
+func (tx *writeTx) Update(r rr.Repository, key rr.Key, update rr.Update) {
+	tx.addOp(r, func(repo *memRepo) {
 		var (
-			k    = table.keyFn(key)
-			item = table.get(k)
+			k    = repo.keyFn(key)
+			item = repo.get(k)
 		)
 		if item == nil {
 			item = key
 		}
-		table.put(k, applyUpdate(item, update))
+		repo.put(k, applyUpdate(item, update))
 	})
 }
 
@@ -113,43 +106,61 @@ func applyUpdate(item rr.Item, update rr.Update) rr.Item {
 	return item
 }
 
-func (tx *writeTx) Delete(t rr.Repository, k rr.Key) {
-	tx.addOp(t, func(table *repoTable) {
-		delete(table.data, table.keyFn(k))
+func (tx *writeTx) Delete(r rr.Repository, k rr.Key) {
+	tx.addOp(r, func(repo *memRepo) {
+		delete(repo.data, repo.keyFn(k))
 	})
 }
 
-func (tx *writeTx) addOp(t rr.Repository, op func(table *repoTable)) {
-	tx.ops[t] = append(tx.ops[t], op)
+func (tx *writeTx) addOp(r rr.Repository, op func(repo *memRepo)) {
+	tx.ops[r] = append(tx.ops[r], op)
 
 }
 
 func (tx *writeTx) Commit(ctx context.Context) error {
-	tables, err := resolveTables(tx.tables, tx.ops)
+	repos, err := resolveRepos(tx.repos, tx.ops)
 	if err != nil {
 		return err
 	}
 	tx.mu.Lock()
 	defer tx.mu.Unlock()
 
-	for table, ops := range tables {
+	for repo, ops := range repos {
 		for _, op := range ops {
-			op(table)
+			op(repo)
 		}
 	}
 	return nil
 }
 
-func resolveTables[T any](
-	tables map[rr.Repository]*repoTable, in map[rr.Repository]T,
-) (map[*repoTable]T, error) {
-	out := make(map[*repoTable]T, len(in))
-	for t, val := range in {
-		table, ok := tables[t]
+type memRepo struct {
+	r     rr.Repository
+	keyFn KeyFunc
+	data  map[string][]byte
+}
+
+func (mr *memRepo) get(key string) (item rr.Item) {
+	if data, ok := mr.data[key]; ok {
+		json.Unmarshal(data, &item) // TODO: don't ignore error
+	}
+	return
+}
+
+func (mr *memRepo) put(key string, item rr.Item) {
+	data, _ := json.Marshal(item) // TODO: don't ignore error
+	mr.data[key] = data
+}
+
+func resolveRepos[T any](
+	repos map[rr.Repository]*memRepo, in map[rr.Repository]T,
+) (map[*memRepo]T, error) {
+	out := make(map[*memRepo]T, len(in))
+	for r, val := range in {
+		repo, ok := repos[r]
 		if !ok {
-			return nil, fmt.Errorf("table not found: %s", t)
+			return nil, fmt.Errorf("repository not found: %s", r)
 		}
-		out[table] = val
+		out[repo] = val
 	}
 	return out, nil
 }
