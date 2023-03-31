da82898309206c87cc1406ecc912df47df429980
Pasi Hyvönen <pasi@koud.fi>
2023-03-31 17:37:53 +0300

    Add value.go with type TODOs

---
+++ rr/value.go
@@ -0,0 +1,12 @@
+package rr
+
+// TODO: []byte
+// TODO: bool
+// TODO: []byte set
+// TODO: list (slice)
+// TODO: map
+// TODO: number
+// TODO: number set
+// TODO: null
+// TODO: string
+// TODO: string set
