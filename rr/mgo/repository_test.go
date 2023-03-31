d37a0deafe2d4c53225b2c4b9887ca90852be65f
Pasi Hyvönen <pasi@koud.fi>
2023-03-31 07:16:59 +0300

    Setup mongo test

---
+++ rr/mgo/repository_test.go
@@ -0,0 +1,30 @@
+package mgo_test
+
+import (
+	"context"
+	"flag"
+	"testing"
+	"time"
+
+	"github.com/koud-fi/pkg/rr/mgo"
+	"github.com/koud-fi/pkg/rr/rrtest"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var connURL = flag.String("url", "mongodb://localhost:27017", "Mongodb connection URL")
+
+func TestRW(t *testing.T) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(*connURL))
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	if err := client.Connect(ctx); err != nil {
+		t.Fatal(err)
+	}
+	rrtest.Run(t, mgo.NewRW(client.Database("test")))
+}
