016aaf3c3e4ad8f23d1d842102ed3910c7dcba6c
Pasi Hyvönen <pasi@koud.fi>
2023-08-02 21:16:15 +0300

    Add some extremely soddy go type generation

---
+++ schema/codegen/gotype_test.go
@@ -0,0 +1,23 @@
+package codegen_test
+
+import (
+	"bytes"
+	"log"
+	"testing"
+
+	"github.com/koud-fi/pkg/schema"
+	"github.com/koud-fi/pkg/schema/codegen"
+)
+
+func TestGoType(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+	if err := codegen.GoType(buf, schema.Resolve[struct {
+		A string
+		B int
+		C float64
+		D bool `json:"_d"`
+	}]().Type); err != nil {
+		log.Fatal(err)
+	}
+	t.Log(buf.String())
+}
