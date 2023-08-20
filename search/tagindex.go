06f286d0bf347dae4fa8a8cafedb1fe6b477aade
Pasi Hyvönen <pasi@koud.fi>
2023-08-20 20:04:00 +0300

    Add QueryResult.Page helper method

--- search/tagindex.go
+++ search/tagindex.go
@@ -1,5 +1,9 @@
 package search
 
+import "strconv"
+
+const cursorBase = 36
+
 type Entry struct {
 	ID    string
 	Order int64
@@ -9,6 +13,26 @@ type Entry struct {
 type QueryResult struct {
 	Data       []Entry
 	TotalCount int
+}
+
+func (qr QueryResult) Page(cursor string, limit int) ([]Entry, string) {
+	var (
+		dataLen    = int64(len(qr.Data))
+		start, _   = strconv.ParseInt(cursor, cursorBase, 64)
+		end        int64
+		nextCursor string
+	)
+	if limit <= 0 {
+		end = dataLen
+	} else {
+		end = start + int64(limit)
+		if end >= dataLen {
+			end = dataLen
+		} else {
+			nextCursor = strconv.FormatInt(end, cursorBase)
+		}
+	}
+	return qr.Data[start:end], nextCursor
 }
 
 type TagInfo struct {
