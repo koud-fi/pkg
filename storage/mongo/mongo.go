b009f169b8189645b96a1c4c9a10e0f78c0cccd1
Pasi Hyvönen <pasi@koud.fi>
2023-09-10 09:36:57 +0300

    Implement mongo Get and Delete

--- storage/mongo/mongo.go
+++ storage/mongo/mongo.go
@@ -1,19 +1,45 @@
 package mongo
 
-import "context"
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
+)
 
 type KV[T any] struct {
-	// TODO
+	coll *mongo.Collection
+}
+
+func NewKV[T any](c *mongo.Collection) *KV[T] {
+	return &KV[T]{coll: c}
 }
 
 func (kv *KV[T]) Get(ctx context.Context, key string) (T, error) {
-	panic("TODO")
+	var d doc[T]
+	err := kv.coll.FindOne(ctx, bson.M{"_id": key}).Decode(&d)
+	return d.Data, err
 }
 
 func (kv *KV[T]) Put(ctx context.Context, key string, value T) error {
+
+	// ???
+
 	panic("TODO")
 }
 
 func (kv *KV[T]) Delete(ctx context.Context, keys ...string) error {
-	panic("TODO")
+	_, err := kv.coll.DeleteMany(ctx, bson.M{"_id": bson.M{
+		"$in": keys,
+	}})
+	return err
+}
+
+type doc[T any] struct {
+	ID        string     `bson:"_id"`
+	Data      T          `bson:"data"`
+	CreatedAt time.Time  `bson:"createdAt"`
+	UpdatedAt *time.Time `bson:"updatedAt,omitempty"`
 }
