c02d9e33c0329ff92fda8c6faf1f548ec37eb6e2
Pasi Hyvönen <pasi@koud.fi>
2024-04-06 14:33:36 +0300

    Add Result.String method

--- num/dice/result.go
+++ num/dice/result.go
@@ -1,6 +1,13 @@
 package dice
 
-import "github.com/koud-fi/pkg/num"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/koud-fi/pkg/num"
+)
+
+var unicodeDie = []rune{'⚀', '⚁', '⚂', '⚃', '⚄', '⚅'}
 
 type Result struct {
 	Die   Die
@@ -8,3 +15,26 @@ type Result struct {
 }
 
 func (r Result) Total() int { return num.Sum(r.Rolls...) }
+
+func (r Result) String() string {
+	var (
+		sb  strings.Builder
+		sum int
+	)
+	for i, roll := range r.Rolls {
+		if r.Die == D6 {
+			if i > 0 {
+				sb.WriteByte(',')
+			}
+			sb.WriteRune(unicodeDie[roll-1])
+		} else {
+			if i > 0 {
+				sb.WriteByte('+')
+			}
+			fmt.Fprint(&sb, roll)
+		}
+		sum += roll
+	}
+	fmt.Fprintf(&sb, " = %d", sum)
+	return sb.String()
+}
