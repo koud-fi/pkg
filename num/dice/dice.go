2df25af4729698d7cd09cd18ebe6d8cf4815a34b
Pasi Hyvönen <pasi@koud.fi>
2024-04-06 14:00:35 +0300

    Add missing dies and make them const

--- num/dice/dice.go
+++ num/dice/dice.go
@@ -10,7 +10,15 @@ import (
 	"github.com/koud-fi/pkg/num"
 )
 
-var D20 Die = 20
+const (
+	D4   Die = 4
+	D6   Die = 6
+	D8   Die = 8
+	D10  Die = 10
+	D12  Die = 12
+	D20  Die = 20
+	D100 Die = 100
+)
 
 type Die int
 
