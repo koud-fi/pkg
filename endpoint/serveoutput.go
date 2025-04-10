94e3b1f13f1ae3a8a507389a6dddc15649dd6542
Pasi Hyvönen <pasi@koud.fi>
2025-04-10 07:24:27 +0300

    Add redirect support for endpoints

--- endpoint/serveoutput.go
+++ endpoint/serveoutput.go
@@ -9,6 +9,15 @@ import (
 	"github.com/koud-fi/pkg/serve"
 )
 
+type redirect struct {
+	URL    string
+	Status int
+}
+
+func Redirect(url string, status int) redirect {
+	return redirect{URL: url, Status: status}
+}
+
 func serveOutput(
 	w http.ResponseWriter, r *http.Request, output any, opts []serve.Option,
 ) (*serve.Info, error) {
@@ -25,6 +34,8 @@ func serveOutput(
 		return serve.Blob(w, r, blob.FromString(v), opts...)
 	case fmt.Stringer:
 		return serve.Blob(w, r, blob.FromString(v.String()), opts...)
+	case redirect:
+		return serve.Redirect(w, r, v.URL, v.Status)
 	default:
 		return serve.JSON(w, r, v, opts...)
 	}
