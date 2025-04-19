9be27a39256c0e06b96bf20192c23672ddef2b2c
Pasi Hyvönen <pasi@koud.fi>
2025-04-19 10:38:00 +0300

    Base for api.ServeMux

---
+++ api/mux.go
@@ -0,0 +1,13 @@
+package api
+
+import "net/http"
+
+type ServeMux struct {
+	*http.ServeMux
+}
+
+func NewServeMux() *ServeMux {
+	return &ServeMux{ServeMux: http.NewServeMux()}
+}
+
+// TODO: methods
