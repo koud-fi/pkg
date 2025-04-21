2581424e5f44feb37412600f7f49a22c7e2789d5
Pasi Hyvönen <pasi@koud.fi>
2025-04-21 16:05:37 +0300

    Add MuxSchema

--- api/schema.go
+++ api/schema.go
@@ -2,17 +2,40 @@ package api
 
 import "github.com/koud-fi/pkg/schema/jsonschema"
 
-type EndpointSchema struct {
-	Input  *jsonschema.Schema `json:"input,omitempty" yaml:"input,omitempty"`
-	Output *jsonschema.Schema `json:"output,omitempty" yaml:"output,omitempty"`
-}
+type (
+	EndpointSchema struct {
+		Name   string             `json:"name,omitempty" yaml:"name,omitempty"`
+		Input  *jsonschema.Schema `json:"input,omitempty" yaml:"input,omitempty"`
+		Output *jsonschema.Schema `json:"output,omitempty" yaml:"output,omitempty"`
+	}
+	MuxSchema struct {
+		Endpoints []MuxEndpointSchema `json:"endpoints" yaml:"endpoints"`
+	}
+	MuxEndpointSchema struct {
+		Name string `json:"name" yaml:"name"`
+		EndpointSchema
+	}
+)
 
-func (e Endpoint) Schema() *EndpointSchema {
+func (e *Endpoint) Schema() EndpointSchema {
 
 	// TODO: this can't really handle all possible output types
 
-	return &EndpointSchema{
+	return EndpointSchema{
 		Input:  jsonschema.FromType(e.inType),
 		Output: jsonschema.FromType(e.outType),
 	}
+}
+
+func (m *Mux) Schema() MuxSchema {
+	schema := MuxSchema{
+		Endpoints: make([]MuxEndpointSchema, 0, len(m.endpoints)),
+	}
+	for name, endpoint := range m.endpoints {
+		schema.Endpoints = append(schema.Endpoints, MuxEndpointSchema{
+			Name:           name,
+			EndpointSchema: endpoint.Schema(),
+		})
+	}
+	return schema
 }
