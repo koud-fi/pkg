0d0ae667af3f2a47bb8bd0db2ec6b88fd35d470f
Pasi Hyvönen <pasi@koud.fi>
2025-04-21 12:54:29 +0300

    Add very handwavy EndpointSchema type

---
+++ api/schema.go
@@ -0,0 +1,18 @@
+package api
+
+import "github.com/koud-fi/pkg/schema/jsonschema"
+
+type EndpointSchema struct {
+	Input  *jsonschema.Schema `json:"input,omitempty" yaml:"input,omitempty"`
+	Output *jsonschema.Schema `json:"output,omitempty" yaml:"output,omitempty"`
+}
+
+func (e endpoint) Schema() *EndpointSchema {
+
+	// TODO: this can't really handle all possible output types
+
+	return &EndpointSchema{
+		Input:  jsonschema.FromType(e.inType),
+		Output: jsonschema.FromType(e.outType),
+	}
+}
