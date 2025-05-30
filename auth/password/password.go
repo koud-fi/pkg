767baf82acd2f9686f159eee891523ba0514ba54
Pasi Hyvönen <pasi@koud.fi>
2025-05-30 07:22:24 +0300

    Add more password options

--- auth/password/password.go
+++ auth/password/password.go
@@ -1,6 +1,9 @@
 package password
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/koud-fi/pkg/auth"
 
 	"golang.org/x/crypto/bcrypt"
@@ -11,18 +14,36 @@ const (
 	MaxCost = bcrypt.MaxCost
 )
 
-var DefaultConfig = Config{
-	Cost: 12,
-}
+var (
+	DefaultConfig = Config{
+		Cost:      12,
+		MinLength: 8,
+	}
+	ErrPasswordTooShort = errors.New("password is too short")
+)
 
 type Hash []byte
 
 type Config struct {
-	Cost int `json:"-"`
+	Cost      int `json:"-"`
+	MinLength int
+
+	// TODO: more config options
 }
 
 func (conf Config) NewHash(plain string) (Hash, error) {
+	if err := conf.Validate(plain); err != nil {
+		return nil, err
+	}
 	return bcrypt.GenerateFromPassword([]byte(plain), conf.Cost)
+}
+
+func (conf Config) Validate(plain string) error {
+	if len(plain) < conf.MinLength {
+		return fmt.Errorf("%w: must be at least %d characters",
+			ErrPasswordTooShort, conf.MinLength)
+	}
+	return nil
 }
 
 func Compare(plain string, to Hash) error {
